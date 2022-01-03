c8621ebbde130f2a852fce8665783bdc5a84c3c1
Dan Lapidos <dlapidos@servicetitan.com>
2022-01-03 14:42:05 -0700

    Add/modify view_routes

---
+++ flyway/sql/V6__create_route_view.sql
@@ -0,0 +1,8 @@
+CREATE VIEW vw_routes AS
+SELECT  tenants.name AS tenant_name,
+        routes.id,
+        routes.tenant_id,
+        routes.name
+FROM tenants
+INNER JOIN routes
+ON tenants.id = routes.tenant_id;
---
+++ flyway/sql/V7__add_route_name_to_route_view.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW vw_routes AS
+SELECT  tenants.name AS tenant_name,
+        routes.id,
+        routes.tenant_id,
+        routes.name,
+        routes.name AS route_name
+FROM tenants
+INNER JOIN routes
+ON tenants.id = routes.tenant_id;
