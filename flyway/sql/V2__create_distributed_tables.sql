94b5b8a04d48a5cc2f08ab9ef1f92bc6cf644be4
Dan Lapidos <dlapidos@servicetitan.com>
2022-01-03 10:03:52 -0700

    Initial sample migrating a database

---
+++ flyway/sql/V2__create_distributed_tables.sql
@@ -0,0 +1,4 @@
+SELECT create_distributed_table('tenants', 'id');
+SELECT create_distributed_table('routes', 'tenant_id');
+SELECT create_distributed_table('route_itineraries', 'tenant_id');
+SELECT create_distributed_table('route_itinerary_tasks', 'tenant_id');
