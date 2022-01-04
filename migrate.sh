165490500bab905b0134b05ce99e19cc3610a81f
Dan Lapidos <dlapidos@servicetitan.com>
2022-01-04 14:21:38 -0700

    Remove container when done

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t flyway-sample:local ./flyway
-docker run --env-file ./postgres.env --network flyway-demo_flyway-network flyway-sample:local 
+docker run --rm --env-file ./postgres.env --network flyway-demo_flyway-network flyway-sample:local 
