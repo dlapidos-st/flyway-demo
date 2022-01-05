b385a1c77eb7309abe6d3a67708c9d94a78088e6
Dan Lapidos <dlapidos@servicetitan.com>
2022-01-05 16:08:42 -0700

    Remove unused files/sections

--- migrate.sh
+++ migrate.sh
@@ -8,8 +8,3 @@ docker run \
     --volume ${PWD}/flyway/sql/:/flyway/sql/ \
     flyway/flyway:8.3.0 \
     migrate
-
-
-
-#docker build -t flyway-sample:local ./flyway
-#docker run --rm --env-file ./postgres.env --network flyway-demo_flyway-network flyway-sample:local 
