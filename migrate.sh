809579bf3ba7b64b6f19fffd3d768e84163490bc
Dan Lapidos <88550064+dlapidos-st@users.noreply.github.com>
2022-01-06 07:12:00 -0700

    Simplify docker (#2)
    * Simplify docker migrations
    
    * Remove unused files/sections
    
    * Updated README.md
    
    * minor README.md changes

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker build -t flyway-sample:local ./flyway
-docker run --rm --env-file ./postgres.env --network flyway-demo_flyway-network flyway-sample:local 
+docker run \
+    --rm \
+    --env-file ./postgres.env \
+    --network flyway-demo_flyway-network \
+    --volume ${PWD}/flyway/conf/:/flyway/conf/ \
+    --volume ${PWD}/flyway/sql/:/flyway/sql/ \
+    flyway/flyway:8.3.0 \
+    migrate
